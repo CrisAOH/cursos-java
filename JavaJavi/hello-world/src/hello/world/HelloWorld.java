d11a3f9319e13f4cbbcd1f6da62766fe0d27e80b
Cristhian Ortega <cristhian.aprendizaje@outlook.com>
2026-06-02 22:39:56 -0600

    Proyecto inicial: Hola Mundo.

---
+++ JavaJavi/hello-world/src/hello/world/HelloWorld.java
@@ -0,0 +1,16 @@
+package hello.world;
+
+/**
+ *
+ * @author Alberto Hernández
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
