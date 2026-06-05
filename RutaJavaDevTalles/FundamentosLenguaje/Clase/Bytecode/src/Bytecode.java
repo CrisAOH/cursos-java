426a9dc437d870894b12b5c53ddaf6472df883f2
Cristhian Ortega <cristhian.aprendizaje@outlook.com>
2026-06-04 19:57:41 -0600

    Revisado el tema de compilación desde terminal.

---
+++ RutaJavaDevTalles/FundamentosLenguaje/Clase/Bytecode/src/Bytecode.java
@@ -0,0 +1,12 @@
+public class Bytecode {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
+
+/*
+ * En la terminal, al ejecutar el comando "javac <archivo-main>.java"
+ * compilará el proyecto y generará el bytecode (archivo .class).
+ *
+ * Para ejecutar el programa, se ejecuta el comando "java <nombre-bytecode>".
+ */
